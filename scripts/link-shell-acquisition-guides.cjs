203723b020d783600e1f9de0327f63d3b0d9ca23
mugecxw-sys <mugecxw@gmail.com>
2026-08-29 00:07:50 +0800

    Add Smert and Eredrim shell guides

--- scripts/link-shell-acquisition-guides.cjs
+++ scripts/link-shell-acquisition-guides.cjs
@@ -9,7 +9,9 @@ let shells = fs.readFileSync(shellsPath, 'utf8');
 shells = shells
   .replace('Reported near Widow’s Overlook.</p>', 'Get the <a href="../../walkthrough/widows-overlook/#tiel-shell">annotated Tiel route</a> in Widow’s Overlook.</p>')
   .replace('Reported near Blackridge Pass.</p>', 'Get the <a href="proxima/">annotated Proxima route</a> at Blackridge Cliffs.</p>')
-  .replace('Reported at the One-Legged Wolf Tavern.</p>', 'Get the <a href="gragu/">annotated Gragu route</a> from the One-Legged Wolf Tavern.</p>');
+  .replace('Reported at the One-Legged Wolf Tavern.</p>', 'Get the <a href="gragu/">annotated Gragu route</a> from the One-Legged Wolf Tavern.</p>')
+  .replace('Reported after the Citadel of Penance Warden.</p>', 'Get the <a href="eredrim/">annotated Eredrim route</a> from Citadel of Penance Beacon.</p>')
+  .replace('Reported at Prophet’s Rest.</p>', 'Get the <a href="smert/">annotated Smert route</a> from Outskirts of Nochte Beacon.</p>');
 fs.writeFileSync(shellsPath, shells);
 
 let widows = fs.readFileSync(widowsPath, 'utf8');
